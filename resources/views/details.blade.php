dcfae20761cb2973d0f874287445a0da8799e89d
Makenley <makenley1@gmail.com>
2018-09-06 16:56:53 -0500

    productDetails

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -16,10 +16,10 @@
 <section class="details mt-4">
 <div class="container-fluid">
 	<div class="row">
-		<div class="col-md-8">
+		<div class="col-md-6">
 		   <div class="product-heading">
-		    <h2>Maruti Ritz ZDi</h2>
-		    <ul class="list-unstyled list-inline">
+		    <h2 class="text-dark">Maruti Ritz ZDi</h2>
+		    <ul class="list-unstyled list-inline text-dark">
 		        <li class="list-inline-item">  Ahmedabad (reg) </li>
 		        <li class="list-inline-item"> | </li>
 		        <li class="list-inline-item">   Second Owner </li>
@@ -115,105 +115,12 @@
     </div>
 </div>
 		   </div>
-		   <div class="cars-overview  card-body pt-3 bg-warning border mt-6">
-		       <div class="row ml-2">
-		           <div class="col-md-12">
-		               <h4>Overview</h4>
-		               
-		           </div>
-		       </div>
-		       <div class="row pt-2">
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-road pr-1"></i> Km's Driven :</li>
-		                   <li class="list-inline-item"> 24000KM</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-calendar-o pr-1"></i> Year :</li>
-		                   <li class="list-inline-item"> 2010</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-car pr-1"></i> Fuel Type :</li>
-		                   <li class="list-inline-item"> Petrol</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-road pr-1"></i> Transmission :</li>
-		                   <li class="list-inline-item"> automatic</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-calendar-o pr-1"></i> Condition :</li>
-		                   <li class="list-inline-item"> Slightly used</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-car pr-1"></i> Body style :</li>
-		                   <li class="list-inline-item">  SUV</li>
-		               </ul>
-		           </div>
-		           
-		       </div>
-		   </div>
-		  <div class="cars-features  card-body pt-3 bg-warning border mt-2">
-		       <div class="row">
-		           <div class="col-md-12">
-		               <h4>Top Specs & Features</h4>
-		               
-		           </div>
-		       </div>
-		       <div class="row pt-2">
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-road pr-1"></i> Km's Driven :</li>
-		                   <li class="list-inline-item"> 24000KM</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-calendar-o pr-1"></i> Year :</li>
-		                   <li class="list-inline-item"> 2010</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-car pr-1"></i> Fuel Type :</li>
-		                   <li class="list-inline-item"> Petrol</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-road pr-1"></i> Transmission :</li>
-		                   <li class="list-inline-item"> automatic</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-calendar-o pr-1"></i> Condition :</li>
-		                   <li class="list-inline-item"> Slightly used</li>
-		               </ul>
-		           </div>
-		           <div class="col-md-4">
-		               <ul class="list-inline list-unstyled">
-		                   <li class="list-inline-item "> <i class="fa fa-car pr-1"></i> Body style :</li>
-		                   <li class="list-inline-item">  SUV</li>
-		               </ul>
-		           </div>
-		           
-		       </div>
-		   </div>
-		   <div class="cars-details   bg-warning border mt-2">
+		  
+		   <div class="cars-details   bg-primary border mt-2">
 		        <div id="accordion">
 
   <div class="card">
-    <div class="card-header bg-warning">
+    <div class="card-header bg-primary">
       <a class="card-link" data-toggle="collapse" href="#collapseOne">
         Specifications      </a>
     </div>
@@ -316,8 +223,8 @@
 		   </div>
 		   
 		</div>
-		<div class="col-md-4">
-		    <div class="cars-overview  card-body pt-3 bg-warning border mt-2">
+		<div class="col-md-5 ml-5 mt-5">
+		    <div class="cars-overview  card-body pt-3 bg-primary border mt-2">
 		       <div class="row">
 		           <div class="col-md-12">
 		               <h4>Overview</h4>
@@ -365,7 +272,7 @@
 		       </div>
 		   </div>
 
-		   <div class="cars-overview  card-body pt-3 bg-warning border mt-2">
+		   <div class="cars-overview  card-body pt-3 bg-primary border mt-2">
 		       <div class="row">
 		           <div class="col-md-12">
 		               <h4>Overview</h4>
@@ -415,92 +322,137 @@
 		</div>
 	</div>
 </div>
-<div class="container">
+ <div class="container-fluid">
     <div class="row mt-3">
         <div class="col-md-12">
             <h3> Find Similar Products</h3>
             <p>Listopia - Directory, Community  Theme</p>
         </div>
     </div>
-    <div class="row mt-2">
-        <div class="col-md-12">
-             <div class="row">
-		<div class="col-md-12">
-		     <div class="row py-2">
-		        <div class="col-md-3 vertical-box">
-		             <div class="card">
-                      <img class="card-img-top mh200-text" src="https://img2.gaadicdn.com/images/usedcarimages/320x224/11-2017/1843403/IMG_6603.jpg" alt="Card image">
-                      <div class="card-body">
-                        <h4 class="card-title mw30-text">2006 Toyota Innova 2.5 V Diesel 7-seater
-                    2006 Toyota Innova 2.5 V Diesel 7-se</h4>
-                    <h5 class="card-text">Rs. 25.32 Lac*</h5>
     
-    <ul class="list-inline">
-                            <li class="list-inline-item">44,114 Km</li>
-                             <li class="list-inline-item">Petrol</li>
-                             <li class="list-inline-item">Gurugram</li>
-                        </ul>
-                        <button type="button" class="btn btn-outline-danger">Contact Sellers</button>
-  </div>
-</div>
-		         </div>
-		        <div class="col-md-3 vertical-box">
-		             <div class="card">
-                      <img class="card-img-top mh200-text" src="https://img2.gaadicdn.com/images/usedcarimages/320x224/11-2017/1843403/IMG_6603.jpg" alt="Card image">
-                      <div class="card-body">
-                        <h4 class="card-title mw30-text">2006 Toyota Innova 2.5 V Diesel 7-seater
-                    2006 Toyota Innova 2.5 V Diesel 7-se</h4>
-                    <h5 class="card-text">Rs. 25.32 Lac*</h5>
-    
-    <ul class="list-inline">
-                            <li class="list-inline-item">44,114 Km</li>
-                             <li class="list-inline-item">Petrol</li>
-                             <li class="list-inline-item">Gurugram</li>
-                        </ul>
-                        <button type="button" class="btn btn-outline-danger">Contact Sellers</button>
-  </div>
-</div>
-		         </div>
-		         <div class="col-md-3 vertical-box">
-		             <div class="card">
-                      <img class="card-img-top mh200-text" src="https://img2.gaadicdn.com/images/usedcarimages/320x224/11-2017/1843403/IMG_6603.jpg" alt="Card image">
-                      <div class="card-body">
-                        <h4 class="card-title mw30-text">2006 Toyota Innova 2.5 V Diesel 7-seater
-                    2006 Toyota Innova 2.5 V Diesel 7-se</h4>
-                    <h5 class="card-text">Rs. 25.32 Lac*</h5>
-    
-    <ul class="list-inline">
-                            <li class="list-inline-item">44,114 Km</li>
-                             <li class="list-inline-item">Petrol</li>
-                             <li class="list-inline-item">Gurugram</li>
-                        </ul>
-                        <button type="button" class="btn btn-outline-danger">Contact Sellers</button>
-  </div>
-</div>
-		         </div>
-		         <div class="col-md-3 vertical-box">
-		             <div class="card">
-                      <img class="card-img-top mh200-text" src="https://img2.gaadicdn.com/images/usedcarimages/320x224/11-2017/1843403/IMG_6603.jpg" alt="Card image">
-                      <div class="card-body">
-                        <h4 class="card-title mw30-text">2006 Toyota Innova 2.5 V Diesel 7-seater
-                    2006 Toyota Innova 2.5 V Diesel 7-se</h4>
-                    <h5 class="card-text">Rs. 25.32 Lac*</h5>
-    
-    <ul class="list-inline">
-                            <li class="list-inline-item">44,114 Km</li>
-                             <li class="list-inline-item">Petrol</li>
-                             <li class="list-inline-item">Gurugram</li>
-                        </ul>
-                        <button type="button" class="btn btn-outline-danger">Contact Sellers</button>
-  </div>
-</div>
-		         </div>
-		        
-		     </div>
-		</div>
-	</div>
-        </div>
-    </div>
+          <div class="col">
+            <div class="row">
+                <div class="col-sm-3 my-2 mr-2">
+                    <div class="box7">
+                        <img src="http://bestjquery.com/tutorial/hover-effect/demo156/images/img-1.jpg" alt="">
+                        <div class="box-content">
+                            <h3 class="title">Williamson</h3>
+                            <span class="post">Web Developer</span>
+                            <ul class="icon">
+                                <li><a href="#" class="fa fa-search"></a></li>
+                                <li><a href="#" class="fa fa-link"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="{{ ('products/details') }}" title="View Product">Maison 4 pièces </a></h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class="row">
+                                <div class="col">
+                                    <p class="btn btn-info">99.00 $</p>
+                                </div>
+                                <div class="col">
+                                    <span class="view-count"> 5 <i class="far fa-eye"></i> </span>
+                                    <span class="like-count"> 5 <i class="far fa-thumbs-up"></i> </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 my-2 mr-2">
+                    <div class="box7">
+                        <img src="http://bestjquery.com/tutorial/hover-effect/demo156/images/img-1.jpg" alt="">
+                        <div class="box-content">
+                            <h3 class="title">Williamson</h3>
+                            <span class="post">Web Developer</span>
+                            <ul class="icon">
+                                <li><a href="#" class="fa fa-search"></a></li>
+                                <li><a href="#" class="fa fa-link"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="{{ ('products/details') }}" title="View Product">Maison 4 pièces </a></h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class="row">
+                                <div class="col">
+                                    <p class="btn btn-info">99.00 $</p>
+                                </div>
+                                <div class="col">
+                                    <span class="view-count"> 5 <i class="far fa-eye"></i> </span>
+                                    <span class="like-count"> 5 <i class="far fa-thumbs-up"></i> </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-3 my-2 mr-2">
+                    <div class="box7">
+                        <img src="http://bestjquery.com/tutorial/hover-effect/demo156/images/img-3.jpg" alt="">
+                        <div class="box-content">
+                            <h3 class="title">Kristiana</h3>
+                            <span class="post">Web Designer</span>
+                            <ul class="icon">
+                                <li><a href="#" class="fa fa-search"></a></li>
+                                <li><a href="#" class="fa fa-link"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="{{ ('products/details') }}" title="View Product">Maison 4 pièces </a></h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class="row">
+                                <div class="col">
+                                    <p class="btn btn-info">99.00 $</p>
+                                </div>
+                                <div class="col">
+                                    <span class="view-count"> 5 <i class="far fa-eye"></i> </span>
+                                    <span class="like-count"> 5 <i class="far fa-thumbs-up"></i> </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-sm-3 my-2 mr-2">
+                    <div class="box7">
+                        <img src="http://bestjquery.com/tutorial/hover-effect/demo156/images/img-3.jpg" alt="">
+                        <div class="box-content">
+                            <h3 class="title">Kristiana</h3>
+                            <span class="post">Web Designer</span>
+                            <ul class="icon">
+                                <li><a href="#" class="fa fa-search"></a></li>
+                                <li><a href="#" class="fa fa-link"></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                    <div class="card">
+                        <div class="card-body">
+                            <h4 class="card-title"><a href="{{ ('products/details') }}" title="View Product">Maison 4 pièces </a></h4>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <div class="row">
+                                <div class="col">
+                                    <p class="btn btn-info">99.00 $</p>
+                                </div>
+                                <div class="col">
+                                    <span class="view-count"> 5 <i class="far fa-eye"></i> </span>
+                                    <span class="like-count"> 5 <i class="far fa-thumbs-up"></i> </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+              </div>
+            </div>
+
+
+
 </div>
 
 <script type="text/javascript">
